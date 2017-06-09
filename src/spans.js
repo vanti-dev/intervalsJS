e485016aca575f763ac60c9152c66d102b94bc8f
Tehmo3 <jasper.b.miles@gmail.com>
2017-06-09 22:42:45 +1000

    really starting to work now :)

--- src/spans.js
+++ src/spans.js
@@ -16,12 +16,18 @@ var _emptyInternalRange = _internalRange([null, null, false, false, true])
 
 class RangeClass {
 
-    constructor(lower=null, upper=null, lowerInc=false, upperInc=false) {
-        this.lower = lower;
-        this.upper = upper;
-        this.lowerInc = lowerInc;
-        this.upperInc = upperInc;
-        this._range = _internalRange([lower, upper, lowerInc, upperInc, false]);
+    constructor(settings = {}) {
+        if (settings.upper && settings.lower && settings.upper < settings.lower) {
+            throw new Error("Upper bound is less than lower bound!")
+        }
+        var settings = {
+            //Setting default values
+            lower: settings.lower || null,
+            upper: settings.upper || null,
+            lowerInc: settings.lowerInc || false,
+            upperInc: settings.upperInc || false
+        }
+        this._range = _internalRange([settings.lower, settings.upper, settings.lowerInc, settings.upperInc, false]);
     }
 
     empty() {
@@ -29,6 +35,69 @@ class RangeClass {
         return this;
     }
 
+    replace(settings = {}) {
+        this._range.lower = settings.lower || this._range.lower;
+        this._range.upper = settings.upper || this._range.upper;
+        this._range.lowerInc = settings.lowerInc || this._range.lowerInc;
+        this._range.upperInc = settings.upperInc || this._range.upperInc;
+        return this;
+    }
+
+    lower() {
+        return this._range.lower || null;
+    }
+
+    upper() {
+        return this._range.upper || null;
+    }
+
+    lowerInc() {
+        return this._range.lowerInc || false;
+    }
+
+    upperInc() {
+        return this._range.upperInc || false;
+    }
+
+    isValidRange(obj) {
+        return obj instanceof RangeClass;
+    }
+
+    isValidScalar(scalar) {
+        return typeof scalar === 'number';
+    }
+
+    // contains(other) {
+    //     if (this.isValidRange(other)) {
+    //         if (!this) {
+    //             return false
+    //         }
+    //         else if (!other || )
+    //     }
+    // }
+
+    startsAfter(other) {
+        if (this.isValidRange(other)) {
+            if (this.lower() === other.lower()) {
+                return other.lower() || this.lower();
+            }
+            else if (this.lower() == null) {
+                return false;
+            }
+            else if (other.lower() == null) {
+                return true;
+            }
+            else {
+                return this.lower() > other.lower()
+            }
+        }
+        else if (this.isValidScalar(other)) {
+            return this.lower() >= other;
+        }
+        else {
+            throw new Error("Unsupported type to test for starts after");
+        }
+    }
 }
 
 module.exports = {
--- test/test_range.js
+++ test/test_range.js
@@ -1,15 +1,52 @@
 var range = require("../src/spans.js");
 var assert = require('chai').assert;
 
-describe('ranges', function() {
-    var emptyIntrange = new range.RangeClass().empty();
+describe('Ranges', function() {
     it('Creates empty intrange', function() {
+        var emptyIntrange = new range.RangeClass().empty();
         assert(typeof emptyIntrange === "object" && emptyIntrange != null, 'Object created');
-        assert(emptyIntrange.lower === null);
-        assert(emptyIntrange.upper === null);
+        assert(emptyIntrange.lower() === null);
+        assert(emptyIntrange.upper() === null);
     });
     it ('tests default bounds', function() {
-        assert(!emptyIntrange.lowerInc);
-        assert(!emptyIntrange.upperInc);
+        var emptyIntrange = new range.RangeClass().empty();
+        assert(!emptyIntrange.lowerInc());
+        assert(!emptyIntrange.upperInc());
     });
+
+    it ('tests bounds intrage', function() {
+        var intrange = new range.RangeClass({lower:1, upper:2});
+        assert(intrange.lower() == 1);
+        assert(intrange.upper() == 2);
+    });
+    it ('tests replace range', function () {
+        var intrange = new range.RangeClass({lower:1, upper:2});
+        intrange.replace({lower:3, upper:10});
+        assert(intrange.lower() == 3);
+        assert(intrange.upper() == 10);
+        assert(!intrange.lowerInc());
+    });
+    it ('tests replace includes', function () {
+        var intrange = new range.RangeClass({lower:1, upper:2});
+        intrange.replace({lowerInc:true, upperInc:true});
+        assert(intrange.upperInc());
+        assert(intrange.lowerInc());
+    });
+
+    it ('tests if the ranges are valid', function () {
+        var emptyIntrange = new range.RangeClass().empty();
+        var intrange = new range.RangeClass({lower:1, upper:2});
+        assert(emptyIntrange.isValidRange(emptyIntrange));
+        assert(intrange.isValidRange(intrange));
+    })
+
+    it ('tests if intrange startsAfter', function () {
+        var intrange = new range.RangeClass({lower:1, upper:2});
+        var startsAfterRange = new range.RangeClass({lower: -2, upper: 10});
+        var intrangeUnbounded = new range.RangeClass().empty();
+        assert(intrange.startsAfter(startsAfterRange));
+        assert(intrange.startsAfter(0));
+        assert(!intrange.startsAfter(2));
+        assert(intrange.startsAfter(intrangeUnbounded));
+    })
 });
