17a1b4a80deefa1464ac22d3c6e2a0c61b6f1ccb
Tehmo3 <jasper.b.miles@gmail.com>
2017-06-18 13:41:18 +1000

    RangeSets getting there

--- src/Types/RangeSet.js
+++ src/Types/RangeSet.js
@@ -13,8 +13,20 @@ class RangeSet {
     return new this.Type();
   }
 
-  isValidRangeset(obj) {
+  _testRangeSetType(item) {
+    if (!this.isValidRangeSet(item)) {
+      throw new Error('Invalid range type');
+    }
+  }
+
+  isValidRangeSet(obj) {
     return obj instanceof this.constructor;
+  }
+
+  _testRangeType(item) {
+    if (!this.isValidRange(item)) {
+      throw new Error('Invalid Range type');
+    }
   }
 
   isValidRange(obj) {
@@ -26,7 +38,8 @@ class RangeSet {
   }
 
   copy() {
-    return new this.constructor(this._list);
+    const newList = this._list.slice();
+    return new this.constructor(newList);
   }
 
   contains(item) {
@@ -73,6 +86,13 @@ class RangeSet {
   add(item) {
     let i = 0;
     const buffer = [];
+    let boolean = true;
+
+    this._testRangeType(item);
+
+    if (item.isEmpty) {
+      return;
+    }
     while (i < this._list.length) {
       const r = this._list[i];
 
@@ -82,13 +102,17 @@ class RangeSet {
         if (buffer.length === 0) {
           this._list.splice(i, 0, item);
         }
+        boolean = false;
         break;
       }
       i += 1;
     }
-    if (buffer.length === 0) {
-      this._list.push(item);
-    } else {
+    if (boolean) {
+      if (buffer.length === 0) {
+        this._list.push(item);
+      }
+    }
+    if (buffer.length !== 0) {
       let j = 0;
       for (j = 0; j < buffer.length; j += 1) {
         item = item.union(buffer[j]);
@@ -102,6 +126,55 @@ class RangeSet {
       upper: this._list[this._list.length - 1].upper,
       upperInc: this._list[this._list.length - 1].upperInc,
     });
+  }
+
+  union(others) {
+    const union = this.copy();
+    let i = 0;
+    for (i = 0; i < others.length; i += 1) {
+      this._testRangeSetType(others[i]);
+      union.add(others[i]);
+    }
+    return union;
+  }
+
+  difference(others) {
+    const difference = this.copy();
+    let i = 0;
+    for (i = 0; i < others.length; i += 1) {
+      this._testRangeSetType(others[i]);
+      difference.remove(others[i]);
+    }
+    return difference;
+  }
+
+  intersection(others) {
+    if (!Array.isArray(others)) {
+      others = [others];
+    }
+    let intersection;
+    let output = this.copy();
+    let i;
+    let j;
+    let k;
+    for (i = 0; i < others.length; i += 1) {
+      this._testRangeSetType(others[i]);
+      intersection = new this.constructor([]);
+
+      for (j = 0; j < output._list.length; j += 1) {
+        for (k = 0; k < others[i]._list.length; k += 1) {
+          // This is being changed here... cant figure out why..
+          intersection.add(output._list[j].intersection(others[i]._list[k]));
+        }
+      }
+
+      if (intersection.isEmpty) {
+        return intersection;
+      }
+
+      output = intersection;
+    }
+    return output;
   }
 }
 
--- test/test_IntRangeSet.js
+++ test/test_IntRangeSet.js
@@ -6,10 +6,26 @@ describe('IntRangeSet', function() {
     let range = new IntRange({ lower: 1, upper :10 });
     let range2 = new IntRange({ lower: 5, upper: 18 });
     let range3 = new IntRange({ lower: 1000, upper: 2000 });
+    let i;
     set.add(range);
     set.add(range2);
     set.add(range3);
     set.remove(range);
-    console.log(set.span());
+    let set2 = new IntRangeSet();
+    set2.add(range);
+    set2.add(range2);
+    set.add(range);
+    for (i = 0; i < set._list.length; i += 1) {
+      console.log(set._list[i]);
+    }
+    console.log('---------------------');
+    // const intersection = set.intersection(set2);
+    // for (i = 0; i < set2._list.length; i += 1) {
+    //   console.log(set2._list[i]);
+    // }
+    // console.log('---------------------');
+    // for (i = 0; i < intersection._list.length; i += 1) {
+    //   console.log(intersection._list[i]);
+    // }
   });
 });
